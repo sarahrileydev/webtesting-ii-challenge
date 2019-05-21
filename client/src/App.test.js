0c31f33ba980b2c0ad2cccf5a629cd03c37ce796
sriley <sarahriley.dev@gmail.com>
2019-05-21 16:22:17 -0400

    added file structure

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import { render, cleanup, fireEvent } from "react-testing-library";
+import "jest-dom/extend-expect";
+
+describe("<App />", () => {
+  it("renders without crashing", () => {
+    render(<App />);
+    cleanup();
+  });
 });
